e716efc9b749e00d03138c4bfaa22ab7316162f3
Mike Duran-Mitchell <mike.mitchell.nyc@gmail.com>
2018-05-22 16:23:06 -0700

    working on desktop

--- utils/download-landsat.js
+++ utils/download-landsat.js
@@ -10,45 +10,42 @@ const reportProgress = new Transform({
   }
 });
 
-const gsURLFixer = url =>
-  str.replace("gs://", "https://storage.googleapis.com/");
+const gzipHandler = (response, outputStream) => {
+  const output = fs.createWriteStream(outputStream);
+
+  switch (response.headers["content-encoding"]) {
+    // or, just use zlib.createUnzip() to handle both cases
+    case "gzip":
+      response.pipe(zlib.createGunzip()).pipe(output);
+      break;
+    case "deflate":
+      response.pipe(zlib.createInflate()).pipe(output);
+      break;
+    default:
+      response.pipe(output);
+      break;
+  }
+};
+
+const gs = "https://storage.googleapis.com/";
 
 const googleDataRequest = https.get(
   "https://storage.googleapis.com/gcp-public-data-landsat/index.csv.gz"
 );
-googleDataRequest.on("response", response => {
-  const output = fs.createWriteStream("../data/landsat8-map-sources/google-landsat8.csv");
-
-  switch (response.headers["content-encoding"]) {
-    // or, just use zlib.createUnzip() to handle both cases
-    case "gzip":
-      response.pipe(zlib.createGunzip()).pipe(output);
-      break;
-    case "deflate":
-      response.pipe(zlib.createInflate()).pipe(output);
-      break;
-    default:
-      response.pipe(output);
-      break;
-  }
-});
+googleDataRequest.on("response", response =>
+  gzipHandler(response, __dirname + "/landsat8/map-sources/google.csv")
+);
 
 const amazonDataRequest = https.get(
   "https://landsat-pds.s3.amazonaws.com/c1/L8/scene_list.gz"
 );
-amazonDataRequest.on("response", response => {
-  const output = fs.createWriteStream("../data/landsat8-map-sources/amazon-landsat8.csv");
+amazonDataRequest.on("response", response =>
+  gzipHandler(response, __dirname + "/landsat8/map-sources/amazon.csv")
+);
 
-  switch (response.headers["content-encoding"]) {
-    // or, just use zlib.createUnzip() to handle both cases
-    case "gzip":
-      response.pipe(zlib.createGunzip()).pipe(output);
-      break;
-    case "deflate":
-      response.pipe(zlib.createInflate()).pipe(output);
-      break;
-    default:
-      response.pipe(output);
-      break;
-  }
-});
+const wrs2ShapefileDataRequest = https.get(
+  "https://landsat.usgs.gov/sites/default/files/documents/wrs2_descending.zip"
+);
+wrs2ShapefileDataRequest.on("response", response =>
+  gzipHandler(response, __dirname + "/landsat8/wrs2-shapefiles/wrs2_descending.zip")
+);
